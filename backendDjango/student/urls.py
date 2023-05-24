ebb7e58f7c30d5ce46b78dae9ac4ddd9452acfec
Hasibul Miraz <hasibul.miraz@gmail.com>
2023-05-25 01:22:04 +0600

    student model and serializer

--- backendDjango/student/urls.py
+++ backendDjango/student/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("testpathstudent", views.testview)
+    path("testpathstudent", views.testview),
+    path("student", views.StudentView),
 ]
--- backendDjango/student/views.py
+++ backendDjango/student/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Student
+from .serializers import StudentSerializer
 
 # Create your views here.
 
@@ -12,3 +14,17 @@ def testview(request):
             'test':'test'
         }
         return Response(data)
+    
+@api_view(["GET","POST"])
+def StudentView(request):
+    if request.method == "GET":
+        quryset = Student.objects.all()
+        serializer = StudentSerializer(quryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == "POST":
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
